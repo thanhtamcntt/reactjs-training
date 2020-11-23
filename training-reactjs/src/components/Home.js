2f497be7f8aaf0832e0ec8271ee8f7fecfa8899a
tam.doan <tam.doan@codecomplete.jp>
2020-11-23 14:14:43 +0700

    state -props

--- training-reactjs/src/components/Home.js
+++ training-reactjs/src/components/Home.js
@@ -1,11 +1,51 @@
 import React from "react";
+import Clothes from "./Clothes";
 
 class Home extends React.Component {
- 
-    render()  {
+
+    constructor(props) {
+        super(props);
+        //Chỉ định một state
+        this.state = { website: "Freetuts.net", index: 1 };
+    }
+
+    countDown() {
+        this.setState({
+          index: this.state.index - 1
+        });
+      }
+      countUp(){
+        this.setState((prevState, props) => {
+          return {
+            index: prevState.index + 1
+          }
+        });
+      }
+
+    render() {
         return (
             <div>
                 <h2>Home</h2>
+                <Clothes name="Quần jean" type="Skinny" color="Đen" size="L">Clothes 1</Clothes>
+                <Clothes name="Váy" type="váy công chúa" color="Trắng" size="M">Clothes 2</Clothes>
+                <hr></hr>
+                <h1>Training reactjs {this.state.website} </h1>
+                <hr></hr>
+                <p>Giá trị {this.state.index}</p>
+                <button className="btn btn-primary"
+                    onClick={() => {
+                        this.countUp()
+                    }}
+                >
+                    Tăng
+                </button>
+                <button className="btn btn-danger"
+                    onClick={() => {
+                        this.countDown()
+                    }}
+                >
+                    Giảm
+                </button>
             </div>
         );
     }
--- training-reactjs/src/components/Register.js
+++ training-reactjs/src/components/Register.js
@@ -5,7 +5,14 @@ class Register extends React.Component {
     render()  {
         return (
             <div>
-                <h2>Register</h2>
+                <h1>Register</h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 col-xs-6">
+                            
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
